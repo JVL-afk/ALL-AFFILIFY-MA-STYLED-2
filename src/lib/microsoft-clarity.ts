a91b4fb5d749b9b10a63634ea232b1aaa0045af5
JVL-afk <andreimiroiu2019@gmail.com>
2025-12-08 17:27:09 +0200

    Update microsoft-clarity.ts

--- src/lib/microsoft-clarity.ts
+++ src/lib/microsoft-clarity.ts
@@ -38,7 +38,7 @@ class MicrosoftClarityService {
         c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
         t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i;
         y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
-      })(window, document, "clarity", "script", this.config.projectId);
+      })(window, document, "clarity", "script", this.config.projectId, {}, {});
 
       console.log('Microsoft Clarity initialized with project ID:', this.config.projectId);
     } catch (error) {
